007b7d60f66aa04f0ae46c4c20175517447f70f1
Abdellah Aitouali <aaitoual@e1r7p8.1337.ma>
2022-10-14 17:55:49 +0100

    working on module 08

--- module08/ex02/MutantStack.hpp
+++ module08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: aaitoual <aaitoual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 14:14:38 by aaitoual          #+#    #+#             */
-/*   Updated: 2022/10/14 16:09:00 by aaitoual         ###   ########.fr       */
+/*   Updated: 2022/10/14 17:55:27 by aaitoual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@
 # include <string>
 # include <stack>
 # include <algorithm>
+# include <vector>
 
 template<typename T>
-class   MutantStack : std::stack<T> {
-    
+class   MutantStack : public std::stack<T, std::deque<int> > {
+	public :
+		c::iterator begin() {return c->begin()};
 };
 
 #endif
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aaitoual <aaitoual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 14:14:11 by aaitoual          #+#    #+#             */
-/*   Updated: 2022/10/14 16:14:31 by aaitoual         ###   ########.fr       */
+/*   Updated: 2022/10/14 17:51:55 by aaitoual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,30 @@ int main()
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
+    std::cout << mstack.top() << std::endl;
+    std::cout << mstack.size() << std::endl;
     //[...]
     mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite)
-    {
-    std::cout << *it << std::endl;
-    ++it;
-    }
-    std::stack<int> s(mstack);
+    // MutantStack<int>::iterator it;
+    // MutantStack<int>::iterator ite = mstack.end();
+    // ++it;
+    // --it;
+    // while (it != ite)
+    // {
+    // std::cout << *it << std::endl;
+    // ++it;
+    // }
+    // std::stack<int> s(mstack);
+    
+
+
+    // std::vector<int> test;
+    // test.push_back (1);
+    // test.push_back (2);
+    // test.push_back (3);
+    // std::vector<int>::iterator it = test.begin();
+    // std::cout << it.base() << std::endl;
+    // std::cout << &test[0] << std::endl;
+    // std::cout << *it << std::endl;
     return 0;
 }
